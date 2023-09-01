ca78dbdc628d2304a7148e9d10711e6457af4fed
Esteban Raffo <e.fraffo@gmail.com>
2023-09-01 19:07:01 -0300

    Desafío Clase 6: products

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ app.listen(port, ()=>console.log("Servidor escuchando en el puerto: ", port));
 
 app.use(express.urlencoded({extended:true}));
 
+app.get("/products", async (req, res) => {
+    try{
+        const manager = new ProductManager("./products.json");
+        const products = await manager.getProducts();
+        const limit = req.query.limit;
+        if(products.length){
+            if(limit){
+                const product_list = products.slice(0, limit);
+                res.send(product_list);
+            }else{
+                res.send(products);
+            }
+        }else{
+            res.send("No se encontraron productos");
+        }
+    }catch(error){
+        console.log(error.message);
+    }
+});
+
+app.get("/products/:id", (req, res) => {
+
+});
